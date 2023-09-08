ad0b81a523beb37cf1ae15989d952e7cf4c5449e
JunJun <rv6049z@gre.ac.uk>
2023-09-08 20:42:17 +0100

    created readme files for each project

--- SA_Event Maker/main.py
+++ SA_Event Maker/main.py
@@ -1,5 +1,21 @@
-#Authorr: Roel-Junior Alejo Viernes 
+#Author: Roel-Junior Alejo Viernes 
+
 from icalendar import *
 from datetime import *
+from tkinter import *
 
+#GUI
+root = Tk()
+root.title("SA Event Maker")
+root.geometry("500x500")
 
+#Variables
+shift_name = StringVar()
+campus = StringVar()
+event_description = StringVar()
+event_start_date = StringVar()
+event_start_time = StringVar()
+event_end_date = StringVar()
+event_end_time = StringVar()
+event_organizer = StringVar()
+event_organizer_email = StringVar()
